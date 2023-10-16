7923dc3f38b490c8825925c186b06a11c642ed0b
Inessa Kim <kim.inessa20@yandex.ru>
2023-10-16 22:26:22 +0300

    fix: исправлены ссылки на внешние сайты

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,39 +1,37 @@
-import { Link } from "react-router-dom";
-
 function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__links">
         <li>
-          <Link
-            to="https://km-og.github.io/how-to-learn/"
+          <a
+            href="https://km-og.github.io/how-to-learn/"
             target="_blank"
             rel="noreferrer"
             className="portfolio__link link"
           >
             Статичный сайт
-          </Link>
+          </a>
         </li>
         <li>
-          <Link
-            to="https://km-og.github.io/russian-travel/"
+          <a
+            href="https://km-og.github.io/russian-travel/"
             target="_blank"
             rel="noreferrer"
             className="portfolio__link link"
           >
             Адаптивный сайт
-          </Link>
+          </a>
         </li>
         <li>
-          <Link
-            to="https://km.og.nomoredomains.monster/sign-up"
+          <a
+            href="https://km.og.nomoredomains.monster/sign-up"
             target="_blank"
             rel="noreferrer"
             className="portfolio__link portfolio__link_order_last link"
           >
             Одностраничное приложение
-          </Link>
+          </a>
         </li>
       </ul>
     </section>
